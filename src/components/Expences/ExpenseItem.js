8ebedbdb5e44130bc220b83efcd1662fe4c281d3
Merve Şahinoğlu <artemiswolfs@gmail.com>
2023-06-09 20:49:49 +0300

    msj eklendı

--- src/components/Expences/ExpenseItem.js
+++ src/components/Expences/ExpenseItem.js
@@ -2,7 +2,7 @@ import React, { useState } from "react";
 import ExpenseDate from "./ExpenseDate";
 import Card from "../UI/Card";
 import "./ExpenseItem.css";
-
+//State less presentation(dump) component.
 function ExpenseItem(props) {
   return (
     <Card className="expense-item">
