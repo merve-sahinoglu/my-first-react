e6d598c901989a0022c924b50a39bdfbe0231a2d
Merve Şahinoğlu <artemiswolfs@gmail.com>
2023-07-25 14:36:16 +0300

    chart component eklendı .İşlevsellik sağlandı.

---
+++ src/components/Chart/Chart.js
@@ -0,0 +1,24 @@
+import React from "react";
+
+import ChartBar from "./ChartBar";
+import "./Chart.css";
+
+const Chart = (props) => {
+  const dataPointValues = props.dataPoints.map((dataPoint) => dataPoint.value);
+  const totalMaxium = Math.max(...dataPointValues);
+
+  return (
+    <div className="chart">
+      {props.dataPoints.map((dataPoints) => (
+        <ChartBar
+          key={dataPoints.label}
+          value={dataPoints.value}
+          maxValue={totalMaxium}
+          label={dataPoints.label}
+        />
+      ))}
+    </div>
+  );
+};
+
+export default Chart;
